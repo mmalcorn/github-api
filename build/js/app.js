ea8e6de0cc4cc3cd703c747651837ee16f45e458
mmalcorn <alcornmeredith@gmail.com>
2016-10-15 12:26:16 -0400

    add another 'get' to backend logic to reference the git repository object

--- build/js/app.js
+++ build/js/app.js
@@ -9,10 +9,13 @@ function Git(){
 
 Git.prototype.getRepos = function(username, displayFunction){
   $.get('https://api.github.com/users/' + username + '?access_token=' + apiKey).then(function(response){
-    displayFunction(username, response.main);
+    displayFunction(username, response.repos_url);
     console.log(response);
   }).fail(function(error){
     $('#repos').text(error.responseJSON.message);
+  });
+  $.get('https://api.github.com/users/' + username + '/repos?access_token=' + apiKey).then(function(response){
+    console.log(response);
   });
 }
 
